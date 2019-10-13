73cfd1063389510cd9d80503ea7c9a0c32be2790
sven4500 <sven4500@mail.ru>
2019-10-13 12:44:13 +0300

    Добавлен элемент управления для удаления последней введённной цифры. Реализована логика введения цифры и удаления цифры.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,13 +16,58 @@ using System.Windows.Shapes;
 namespace Lab2
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// MainWindow.xaml.cs описывает поведение конкретного окна MainWindow.
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void NumericButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button but = sender as Button;
+            if (but == null)
+                return;
+            String num = but.Name.Substring(but.Name.Length - 1);
+            TextBox expr = this.FindName("Expr") as TextBox;
+            if (expr == null)
+                return;
+            if (expr.Text == "0")
+            {
+                if (num != "0")
+                {
+                    expr.Text = num;
+                }
+            }
+            else
+            {
+                expr.Text += num;
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button but = sender as Button;
+            if (but == null)
+                return;
+            TextBox expr = this.FindName("Expr") as TextBox;
+            if (expr == null)
+                return;
+            if (expr.Text.Length == 1)
+            {
+                expr.Text = "0";
+            }
+            else
+            {
+                expr.Text = expr.Text.Substring(0, expr.Text.Length - 1);
+            }
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
